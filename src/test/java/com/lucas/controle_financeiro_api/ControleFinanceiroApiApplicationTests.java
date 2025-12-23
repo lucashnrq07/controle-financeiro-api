5a3f7301cf7ae4d36657cbfbfca28b4a74e60c45
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-23 09:39:11 -0300

    primeiro commit

---
+++ src/test/java/com/lucas/controle_financeiro_api/ControleFinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.controle_financeiro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
