abeeafaf69e6039707d1932cddb93c28ad4e9363
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-23 14:48:34 -0300

    retirando métodos desnecessários dos repositories

--- src/main/java/com/lucas/controle_financeiro_api/repositories/CategoryRepository.java
+++ src/main/java/com/lucas/controle_financeiro_api/repositories/CategoryRepository.java
@@ -10,8 +10,5 @@ import java.util.Optional;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Override
-    Optional<Category> findById(Long aLong);
-
     List<Category> findByUserIdOrIsDefaultTrue(Long userId);
 }
