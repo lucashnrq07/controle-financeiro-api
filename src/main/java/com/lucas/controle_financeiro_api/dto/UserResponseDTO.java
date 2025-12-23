8b4f2a74f613691c7f7d05e607b6c5f351c79cf4
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-23 15:00:15 -0300

    correção de resposta ao criar usuário

---
+++ src/main/java/com/lucas/controle_financeiro_api/dto/UserResponseDTO.java
@@ -0,0 +1,13 @@
+package com.lucas.controle_financeiro_api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserResponseDTO(
+   @NotBlank
+   String name,
+
+   @NotBlank
+   @Email
+   String email
+) {}
