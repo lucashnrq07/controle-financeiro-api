55727fadf12c36d06395510ae5652caff5296b4c
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-23 15:00:50 -0300

    correção de resposta ao criar usuário

--- src/main/java/com/lucas/controle_financeiro_api/service/UserService.java
+++ src/main/java/com/lucas/controle_financeiro_api/service/UserService.java
@@ -2,6 +2,7 @@ package com.lucas.controle_financeiro_api.service;
 
 import com.lucas.controle_financeiro_api.domain.entities.User;
 import com.lucas.controle_financeiro_api.dto.UserDTO;
+import com.lucas.controle_financeiro_api.dto.UserResponseDTO;
 import com.lucas.controle_financeiro_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,9 @@ public class UserService {
     private UserRepository repository;
 
     // CREATE NEW USER
-    public UserDTO createUser(UserDTO data) {
+    public UserResponseDTO createUser(UserDTO data) {
         User newUser = this.repository.save(new User(null, data.name(), data.email(), data.password()));
-        return new UserDTO(data.name(), data.email(), data.password());
+        return new UserResponseDTO(data.name(), data.email());
     }
 
     // FIND USER BY ID
