3fcb41cef8d2f92c18da20b9426bdf04f366d3fe
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-23 12:10:15 -0300

    criar controller de User

---
+++ src/main/java/com/lucas/controle_financeiro_api/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.lucas.controle_financeiro_api.controllers;
+
+import com.lucas.controle_financeiro_api.dto.user.UserDTO;
+import com.lucas.controle_financeiro_api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/users")
+@Tag(name = "User", description = "API de gerenciamento de usuários")
+public class UserController {
+
+    private UserService userService;
+
+    // CREATE USER
+    @Operation(summary = "Criar um novo usuário")
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO dto) {
+        UserDTO createdUser = userService.createUser(dto);
+        return ResponseEntity.status(201).body(createdUser);
+    }
+}
