285821639052523373cc7ee23b8017510b462880
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-01-28 18:29:23 -0300

    corrige e prepara UserRepository

--- src/main/java/com/lucas/controle_financeiro_api/repositories/UserRepository.java
+++ src/main/java/com/lucas/controle_financeiro_api/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.lucas.controle_financeiro_api.repositories;
 
 import com.lucas.controle_financeiro_api.domain.entities.User;
+import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Id> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findById(Long userId);
+
+    void deleteById(Long id);
 }
