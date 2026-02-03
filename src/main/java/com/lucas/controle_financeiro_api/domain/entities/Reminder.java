5629b67a2a635fabbebd8a32849eb0d3c444f016
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-02-03 13:44:11 -0300

    cria entidade de lembretes

---
+++ src/main/java/com/lucas/controle_financeiro_api/domain/entities/Reminder.java
@@ -0,0 +1,29 @@
+package com.lucas.controle_financeiro_api.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "movements")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Reminder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDate reminderDate;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    private User user;
+}
