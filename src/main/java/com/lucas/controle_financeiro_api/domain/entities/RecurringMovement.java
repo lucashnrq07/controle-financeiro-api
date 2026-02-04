a3aecb1ffe5541145554743ab6550e8f1e0dd955
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-02-03 23:22:46 -0300

    correção na criação de tabelas

--- src/main/java/com/lucas/controle_financeiro_api/domain/entities/RecurringMovement.java
+++ src/main/java/com/lucas/controle_financeiro_api/domain/entities/RecurringMovement.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "movements")
+@Table(name = "recurring_movements")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
--- src/main/java/com/lucas/controle_financeiro_api/domain/entities/Reminder.java
+++ src/main/java/com/lucas/controle_financeiro_api/domain/entities/Reminder.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "movements")
+@Table(name = "reminders")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
